7ab98743ad7303b7c2b36fb1fca79aa5433825da
arbazkhan123-s <83135990+arbazkhan123-s@users.noreply.github.com>
2021-05-31 13:11:13 +0530

    initial portfolio
    hello

---
+++ index.js
@@ -0,0 +1,15 @@
+function onn() {
+    alert('welcome you are online')
+};
+window.addEventListener("online", onn);
+
+function off() {
+    alert("plz check you are offline")   
+}
+window.addEventListener("offline", off)
+
+
+
+
+
+
